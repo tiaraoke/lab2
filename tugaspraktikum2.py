dc77ad92b8c422559cdb8ee22213d4a3325f6a4d
tiaraoke <oktapianatiara@gmail.com>
2021-11-15 19:00:22 +0700

    tambahkan tugaspraktikum2

---
+++ tugaspraktikum2.py
@@ -0,0 +1,12 @@
+#Menampilkan bilangan terbesar
+
+a  =  input ( "Masukan Bilangan A : " )
+b  =  input ( "Masukan Bilangan B : " )
+c  =  input ( "Masukan Bilangan C : " )
+
+if  a  >  b  and  a  >  c :
+    print ( "Maka Bilangan A yang terbesar" )
+elif  b  >  c  and  b  >  a :
+    print ( "Maka Bilangan B yang terbesar" )
+else :
+    print ( "Maka Bilangan C yang terbesar" )
